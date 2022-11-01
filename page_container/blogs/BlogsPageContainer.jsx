3eb2ebe83f91d555e3d4c1ffc5a75e6915caf0e7
asharinovaldy <asharinovaldi6319@gmail.com>
2022-11-01 17:32:23 +0800

    BW-199 [Task][FE][Desktop][Mobile][KamiUMKM] Improve Categories on Blog List - get data blog categories from API - filter blog by category

--- page_container/blogs/BlogsPageContainer.jsx
+++ page_container/blogs/BlogsPageContainer.jsx
@@ -5,10 +5,11 @@ import BlogsPageContainerMobile from './BlogsPageContainerMobile';
 import { fetchBlogs } from '../../hooks/useBlogs';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { fetchBlogCategories } from '../../hooks/useBlogCategories';
 
 function BlogsPageContainer(props) {
   const { isMobile, query } = props;
-  const { page, perPage } = query;
+  const { category, page, perPage } = query;
   const Router = useRouter();
 
   const {
@@ -18,29 +19,58 @@ function BlogsPageContainer(props) {
     isFetching: isFetchingBlogs,
     isSuccess: isSuccessBlogs,
     refetch: refetchBlogs,
-  } = useQuery(['blogs'], () => fetchBlogs({ page, perPage, sort: 'desc' }), {
+  } = useQuery(
+    ['blogs'],
+    () => fetchBlogs({ page, perPage, sort: 'desc', category }),
+    {
+      staleTime: 0,
+    },
+  );
+
+  const {
+    data: dataBlogCategories,
+    isError: isErrorBlogCategories,
+    isLoading: isLoadingBlogCategories,
+    isFetching: isFetchingCategories,
+    isSuccess: isSuccessBlogCategories,
+  } = useQuery(['blogCategories'], () => fetchBlogCategories(), {
     staleTime: 0,
   });
 
   function fetchPagination(page) {
     page = JSON.stringify(page);
-    Router.push(`/blog?page=${page || 1}&perPage=${perPage || `9`}`);
+    Router.push(
+      `/blog?category=${category || ''}&page=${page || 1}&perPage=${
+        perPage || `9`
+      }`,
+    );
+  }
+
+  function fetchResetBlogs() {
+    Router.push(`/blog?category=&page=1&perPage=9`);
   }
 
   useEffect(() => {
-    if (page || perPage) {
+    if (page || perPage || category) {
       refetchBlogs();
     }
-  }, [page, perPage]);
+  }, [page, perPage, category]);
 
   props = {
     ...props,
+    category,
     dataBlogs,
     isErrorBlogs,
     isLoadingBlogs,
     isFetchingBlogs,
     isSuccessBlogs,
+    dataBlogCategories,
+    isErrorBlogCategories,
+    isLoadingBlogCategories,
+    isFetchingCategories,
+    isSuccessBlogCategories,
     fetchPagination,
+    fetchResetBlogs,
   };
 
   if (isMobile) {
